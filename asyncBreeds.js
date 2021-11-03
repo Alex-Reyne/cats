9e1553919b238e04a46978257fc4615675a23dec
Alex-Reyne <creativereyne@gmail.com>
2021-11-02 20:07:34 -0600

    create tests for syncBreeds and asyncBreeds. Implement extra functionality to both for when breeds are undefined or errors occur

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,14 +2,16 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, whenDone) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+  // console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data");
-
+    // console.log("In readFile's Callback: it has the data");
+    if(error) whenDone(undefined);
     if(!error) whenDone(data);
   });
 };
 
 breedDetailsFromFile('Bombay', printCatBreed = breed => {
-  console.log('return value: ', breed);
+  // console.log('return value: ', breed);
 });
+
+module.exports = breedDetailsFromFile;
