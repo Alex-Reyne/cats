436440be88b7374ac9adeb0e704bbc1e21afa7c9
Alex-Reyne <creativereyne@gmail.com>
2021-11-02 19:31:21 -0600

    clean up code

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, whenDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data");
+
+    if(!error) whenDone(data);
+  });
+};
+
+breedDetailsFromFile('Bombay', printCatBreed = breed => {
+  console.log('return value: ', breed);
+});
